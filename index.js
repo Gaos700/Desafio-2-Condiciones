37e3683dc55157af7034fa9d120100712467c24b
G4os <gustavorellana700@gmail.com>
2025-08-31 22:25:22 -0400

    ejercicio 3

--- index.js
+++ index.js
@@ -24,5 +24,19 @@ botonenviar.addEventListener("click", () =>{
         resultado.textContent = "La suma es " + suma + " estas bien";
         resultado.style.color = "green"
     }
+});
+
+function verificarPassWord(){
+    const d1 = document.getElementById("digito1").value;
+    const d2 = document.getElementById("digito2").value;
+    const d3 = document.getElementById("digito3").value;
+    const password = d1 + d2 +d3;
+    const resultado= document.getElementById("resultado3");
+    if (password === "911"){
+        resultado.textContent = "password 1 correcta";
+    } else if (password === "714"){
+        resultado.textContent = "password 2 correcta";
+    } else {
+        resultado.textContent = "password incorrecta";
+    }
 }
-)
